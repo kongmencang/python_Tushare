3d2c1948e0324771a0475be4c5230f76b64c632b
kongmenchang <1184507696@qq.com>
2024-05-20 20:44:00 +0800

    重构分析base类 新增运营能力指标

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ calculate.get_profitability_metrics_to_excel(PERIOD)
 calculate.get_operational_capability_indicators_to_excel(PERIOD)
 calculate.get_liquidity_ratio_to_excle(PERIOD)
 calculate.get_growth_capacity_indicators_to_excel(PERIOD)
-#wanc
+#
